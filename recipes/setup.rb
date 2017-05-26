799c53abd4d73a7df00ef15ee16c31b91f8d6361
vagrant <bashar.alkhateeb@gmail.com>
2017-05-26 21:34:48 +0000

    ohai

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -12,8 +12,14 @@ package 'git' do
 	action :install
 end
 
+
 file '/etc/motd' do
-	content 'his server is the property of Bashar '
+	content "his server is the property of Bashar 
+	HOSTNAME : #{node['hostname']}
+	IPADDRESS:#{node['ipaddress']}
+	CUP: #{node['cpu']['0']['mhz']}
+	MEMMORY:#{node['memory']['total']}
+"
 	owner 'root'
 	group 'root'
 	action :create
